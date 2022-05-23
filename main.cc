3d09d9b57130ac80add45a4d0e2cf357ee4bb95e
FearIndigo <josephm@fearindigo.com.au>
2022-05-23 23:39:39 +0800

    -test

--- main.cc
+++ main.cc
@@ -16,35 +16,13 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
-static void DrawOnCanvas(Canvas *canvas) {
-  /*
-   * Let's create a simple animation. We use the canvas to draw
-   * pixels. We wait between each step to have a slower animation.
-   */
-  canvas->Fill(0, 0, 255);
-
-  int center_x = canvas->width() / 2;
-  int center_y = canvas->height() / 2;
-  float radius_max = canvas->width() / 2;
-  float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
-    if (interrupt_received)
-      return;
-    float dot_x = cos(a * 2 * M_PI) * r;
-    float dot_y = sin(a * 2 * M_PI) * r;
-    canvas->SetPixel(center_x + dot_x, center_y + dot_y,
-                     255, 0, 0);
-    usleep(1 * 1000);  // wait a little to slow down things.
-  }
-}
-
 int main(int argc, char *argv[]) {
-  //int fd;
+  int fd;
 
-  //if((fd=serialOpen("/dev/ttyACM0",9600))<0){
-  //  fprintf(stderr,"Unable to open serial device: %s\n",strerror(errno));
-  //  return 1;
-  //}
+  if((fd=serialOpen("/dev/ttyACM0",9600))<0){
+    fprintf(stderr,"Unable to open serial device: %s\n",strerror(errno));
+    return 1;
+  }
   
   RGBMatrix::Options defaults;
   defaults.hardware_mapping = "adafruit-hat-pwm";
@@ -63,12 +41,11 @@ int main(int argc, char *argv[]) {
   signal(SIGTERM, InterruptHandler);
   signal(SIGINT, InterruptHandler);
 
-  //for (;;){
-  //  putchar(serialGetchar(fd));
-  //  fflush(stdout);
-  //}
-
-	DrawOnCanvas(canvas);    // Using the canvas.
+  for (;;){
+  	if (interrupt_received)
+  	  return;
+    canvas->SetPixel(32,16,0,0,serialGetchar(fd)=='1'?255:0);
+  }
 
   // Animation finished. Shut down the RGB matrix.
   canvas->Clear();
