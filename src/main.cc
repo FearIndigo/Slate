8ce852b7835b47ede32281da0bbb246f79c18309
FearIndigo <josephm@fearindigo.com.au>
2022-05-23 16:16:42 +0800

    -use correct baud rate for serial read on raspberry pi

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,7 @@
 ﻿import serial, string
 
 output = " "
-ser = serial.Serial('/dev/ttyUSB0', 4800, 8, 'N', 1, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
 while True:
   print "----"
   while output != "":
