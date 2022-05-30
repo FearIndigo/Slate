ba10938fb2da9cb3c4cf0c170491352adf0466cf
FearIndigo <josephm@fearindigo.com.au>
2022-05-30 18:28:20 +0800

    -test

--- src/games/ponglord/main.cpp
+++ src/games/ponglord/main.cpp
@@ -8,12 +8,6 @@ namespace Ponglord
         color = {128,128,128};
 
         // Load font
-        rgb_matrix::Font font2;
-        font = font2;
-        if (!font.LoadFont("4x6.bdf")) {
-            fprintf(stderr, "Couldn't load font '%s'\n", "4x6.bdf");
-        }
-
         if (!font.LoadFont("matrix/fonts/4x6.bdf")) {
             fprintf(stderr, "Couldn't load font '%s'\n", "matrix/font/4x6.bdf");
         }
