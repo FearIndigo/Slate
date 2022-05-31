33ed47af9cf170853d1b3c5f41e42c157f2743ee
FearIndigo <josephm@fearindigo.com.au>
2022-05-31 15:56:04 +0800

    -test

--- main.cpp
+++ main.cpp
@@ -52,9 +52,9 @@ int main(int argc, char *argv[]) {
 				pong.Display(display.canvas);
 
 				// Run the current game when either player long presses both buttons
-				//if((input.GetButtonLongPress(0) && input.GetButtonLongPress(1)) ||
-				//	(input.GetButtonLongPress(2) && input.GetButtonLongPress(3)))
-					//pong.isRunning = true;
+				if((input.GetButtonLongPress(0) && input.GetButtonLongPress(1)) ||
+					(input.GetButtonLongPress(2) && input.GetButtonLongPress(3)))
+						pong.isRunning = true;
 			}
   		}
 	}
