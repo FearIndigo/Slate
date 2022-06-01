ac1b66cfdf84b1404e928e0b0d68a585dde8fc78
FearIndigo <josephm@fearindigo.com.au>
2022-06-01 18:55:10 +0800

    -fix

--- src/core/systems/moveSystem.cpp
+++ src/core/systems/moveSystem.cpp
@@ -6,7 +6,7 @@ namespace Slate
     {
         // Loop through entities with Position and Velocity components
         registry.view<Position,Velocity>()
-        .each([](auto &pos, auto &vel)
+        .each([&frame_time](auto &pos, auto &vel)
         {
             // Update entity position given its velocity
             pos.x += vel.x * frame_time;
