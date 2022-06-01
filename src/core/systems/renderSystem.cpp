2b0ddc1c288c53154057bda2f57475b813649f4c
FearIndigo <josephm@fearindigo.com.au>
2022-06-01 18:59:19 +0800

    -fix

--- src/core/systems/renderSystem.cpp
+++ src/core/systems/renderSystem.cpp
@@ -2,11 +2,11 @@
 
 namespace Slate
 {
-    RenderSystem::Update(entt::registry &registry, rgb_matrix::FrameCanvas *canvas)
+    void RenderSystem::Update(entt::registry &registry, rgb_matrix::FrameCanvas *canvas)
     {
         // Loop through entities with Renderable and Position components
         registry.view<Renderable,Position>()
-        .each([](auto &rend, auto &pos)
+        .each([&canvas](auto &rend, auto &pos)
         {
             // Draw pixels to canvas
             rend.Draw(canvas, pos);
