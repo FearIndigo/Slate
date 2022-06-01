3e47863899156beabe768d8bde3b740556fc39fc
FearIndigo <josephm@fearindigo.com.au>
2022-06-01 18:43:43 +0800

    -fix

--- src/core/components/renderable.cpp
+++ src/core/components/renderable.cpp
@@ -3,11 +3,11 @@
 namespace Slate
 {
     Renderable::Renderable(const std::vector<Slate::Pixel> default_pixels)
-        : pixels(p) {}
+        : pixels(default_pixels) {}
 
     void Draw(rgb_matrix::FrameCanvas *canvas, const Slate::Position pos)
     {
-        for (p : pixels)
+        for (Slate::Pixel p : pixels)
             canvas->SetPixel(pos.x + p.rel_x, pos.y + p.rel_y, p.r, p.g, p.b);
     }
 }
