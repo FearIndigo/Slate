573f7e1b9e92f62b5f5e1ccd28d8f12838a86479
FearIndigo <josephm@fearindigo.com.au>
2022-06-02 21:10:50 +0800

    -fix

--- src/games/ponglord/components/paddle.hpp
+++ src/games/ponglord/components/paddle.hpp
@@ -20,11 +20,10 @@ namespace Ponglord
         /// Constructor.
         ///
         /// \param p1 Does the paddle belong to player 1.
-        /// \param s The starting score for this paddle.
         /// \param b The collisions bounds of the paddle relative to its position.
         /// \param pos The paddle's position component.
         ///
-        Paddle(const bool p1, const unsigned int s, const Ponglord::Bounds b, Slate::Position &pos);
+        Paddle(const bool p1, const Ponglord::Bounds b, Slate::Position &pos);
 
         Paddle(Paddle&&) noexcept;
         
