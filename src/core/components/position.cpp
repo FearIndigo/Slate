dd29ffee42499bf0ff0c1a9897ea1df88cef6b58
FearIndigo <josephm@fearindigo.com.au>
2022-06-02 17:13:23 +0800

    -fix

--- src/core/components/position.cpp
+++ src/core/components/position.cpp
@@ -5,12 +5,12 @@ namespace Slate
     Position::Position(const double init_x, const double init_y)
         : x(init_x), y(init_y) {}
     
-    int Position::MatrixX()
+    int Position::MatrixX() const
     {
         return (int)(x > 0 ? x + 0.5 : x - 0.5);
     }
 
-    int Position::MatrixY()
+    int Position::MatrixY() const
     {
         return (int)(y > 0 ? y + 0.5 : y - 0.5);
     }
