b58914fd2579030d928a1caeaae91a1e4655e418
FearIndigo <josephm@fearindigo.com.au>
2022-06-19 12:10:23 +0800

    -fix type

--- src/core/time.hpp
+++ src/core/time.hpp
@@ -15,7 +15,7 @@ namespace Slate
         ///
         unsigned int Update();
     private:
-        const int max_frame_time;
+        const unsigned int max_frame_time;
     	unsigned long current_time;
     	unsigned long new_time;
     	unsigned int frame_time;
